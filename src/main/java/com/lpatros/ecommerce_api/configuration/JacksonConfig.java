a4e08681fe223e526b48e5866cd10244c7dafeab
lpatros <leopatrosilva@gmail.com>
2026-09-11 16:26:02 -0300

    refactor(config): enhance Jackson datetime serialization module

--- src/main/java/com/lpatros/ecommerce_api/configuration/JacksonConfig.java
+++ src/main/java/com/lpatros/ecommerce_api/configuration/JacksonConfig.java
@@ -1,34 +1,50 @@
 package com.lpatros.ecommerce_api.configuration;
 
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfig {
 
-    private static final String DATE_FORMAT = "dd/MM/yyyy";
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    private static final String DATETIME_FORMAT = "dd/MM/yyyy HH:mm:ss";
+    private static final DateTimeFormatter DATETIME_FORMATTER =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
     @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+    public SimpleModule javaTimeModule() {
+        SimpleModule module = new SimpleModule();
 
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
+        module.addSerializer(
+                LocalDate.class,
+                new LocalDateSerializer(DATE_FORMATTER)
+        );
 
-        return builder -> {
-            builder.serializers(new LocalDateSerializer(dateFormatter));
-            builder.serializers(new LocalDateTimeSerializer(dateTimeFormatter));
+        module.addDeserializer(
+                LocalDate.class,
+                new LocalDateDeserializer(DATE_FORMATTER)
+        );
 
-            builder.deserializers(new LocalDateDeserializer(dateFormatter));
-            builder.deserializers(new LocalDateTimeDeserializer(dateTimeFormatter));
-        };
+        module.addSerializer(
+                LocalDateTime.class,
+                new LocalDateTimeSerializer(DATETIME_FORMATTER)
+        );
+
+        module.addDeserializer(
+                LocalDateTime.class,
+                new LocalDateTimeDeserializer(DATETIME_FORMATTER)
+        );
+
+        return module;
     }
 }
