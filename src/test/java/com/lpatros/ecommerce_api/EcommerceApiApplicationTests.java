33c4f4382dae23eb9d47996a18f1d0e2ce7c460e
lpatros <leopatrosilva@gmail.com>
2026-09-23 14:53:42 -0300

    test(app): smoke test context with containerized Postgres
    - Extend the support base class and activate the test profile

--- src/test/java/com/lpatros/ecommerce_api/EcommerceApiApplicationTests.java
+++ src/test/java/com/lpatros/ecommerce_api/EcommerceApiApplicationTests.java
@@ -1,10 +1,9 @@
 package com.lpatros.ecommerce_api;
 
+import com.lpatros.ecommerce_api.support.PostgresTestContainerSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EcommerceApiApplicationTests {
+class EcommerceApiApplicationTests extends PostgresTestContainerSupport {
 
 	@Test
 	void contextLoads() {
